294d75fd299f11f6e923820af34d5deda319d74d
tizianoGuadagnino <frevo93@gmail.com>
2024-04-15 13:59:20 +0200

    Update LICENCE for opencv, ready to merge

--- cpp/3rdparty/opencv/opencv.cmake
+++ cpp/3rdparty/opencv/opencv.cmake
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2024 Meher Malladi, Tiziano Guadagnino
+# Copyright (c) 2024 Tiziano Guadagnino, Meher Malladi, Saurabh Gupta
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
