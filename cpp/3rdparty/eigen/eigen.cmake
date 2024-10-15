9f72886d179430fe63b288de6f403d8064566c7e
ssg1002 <sgupta2@uni-bonn.de>
2024-10-15 17:00:01 +0200

    apply patch to fix blas, from KISS-ICp

--- cpp/3rdparty/eigen/eigen.cmake
+++ cpp/3rdparty/eigen/eigen.cmake
@@ -27,7 +27,9 @@ set(EIGEN_BUILD_BLAS OFF CACHE BOOL "Don't build blas module")
 set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
 
 include(FetchContent)
-FetchContent_Declare(eigen URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz)
+FetchContent_Declare(
+  eigen URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+  PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/eigen.patch UPDATE_DISCONNECTED 1)
 FetchContent_GetProperties(eigen)
 if(NOT eigen_POPULATED)
   FetchContent_Populate(eigen)
